00f800120abfebf018a32fcc7dfc573d3720471c
ChaitanyaChandra <chay@outlook.in>
2022-07-05 12:53:44 +0530

    automated commit #1183

--- data.tf
+++ data.tf
@@ -2,8 +2,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket = "terraform-nonprod-state-chaitu-env"
-    key    = "vpc/${var.ENV}/alb/terraform.tfstate"
+    key    = "state/${var.ENV}/vpc/terraform.tfstate"
     region = "us-east-1"
   }
 }
-
